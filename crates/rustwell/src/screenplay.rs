48f20cf7a3ebee6e74fbae26be0aa72ee9937395
Fredrik Blomqvist <scanmar333@gmail.com>
2025-10-16 20:57:59 +0200

    complete ast

--- crates/rustwell/src/screenplay.rs
+++ crates/rustwell/src/screenplay.rs
@@ -1,19 +1,40 @@
 use crate::rich_string::RichString;
 
 pub struct Screenplay {
+    pub titlepage: Option<TitlePage>,
     pub elements: Vec<Element>,
 }
 
 pub enum Element {
-    Heading(String),
-    Action(RichString),
-    Dialogue {
-        character: String,
-        elements: Vec<DialogueElement>,
+    Heading {
+        slug: RichString,
+        number: Option<String>,
     },
+    Action(RichString),
+    Dialogue(Dialogue),
+    DualDialogue(Dialogue, Dialogue),
+    Lyrics(RichString),
+    Transition(RichString),
+    CenteredText(RichString),
+    Note(RichString),
+    PageBreak,
+}
+
+pub struct Dialogue {
+    pub character: RichString,
+    pub elements: Vec<DialogueElement>,
 }
 
 pub enum DialogueElement {
     Paranthetical(RichString),
     Line(RichString),
+}
+
+pub struct TitlePage {
+    pub title: Vec<RichString>,
+    pub credit: Vec<RichString>,
+    pub authors: Vec<RichString>,
+    pub source: Vec<RichString>,
+    pub draft_date: Vec<RichString>,
+    pub contact: Vec<RichString>,
 }
