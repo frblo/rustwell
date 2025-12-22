fa49c322a49b2f7fe32071168801f1d687352a76
Fredrik Blomqvist <scanmar333@gmail.com>
2025-12-22 15:32:17 +0100

    Write lib documentation

--- crates/rustwell/src/lib.rs
+++ crates/rustwell/src/lib.rs
@@ -1,3 +1,37 @@
+//! This crate is an implementation of the [fountain](https://fountain.io/) screenplay markup
+//! language specification, and acts as a tool for parsing and exporting the `fountain`
+//! language to different kinds of formatted documents, such as `html` and `pdf`.
+//!
+//! The crate can be used both for the whole compilation process, but also as an intermediary step
+//! for parsing `fountain` text into an abstract syntax tree (AST) that can then be used when
+//! constructing a new exporter.
+//!
+//! # Example
+//!
+//! ```rust
+//! use std::io::BufWriter;
+//! use std::io::stdout;
+//!
+//! fn main() {
+//!     let script = r#"
+//!     Title: Example Screenplay
+//!
+//!     INT. HOUSE – DAY
+//!
+//!     Someone is knocking on the door.
+//!
+//!     GUEST
+//!     (tired)
+//!     Hey, open up!
+//!     "#;
+//!
+//!     let parsed = parse_str(script);
+//!     let mut output = BufWriter::new(stdout());
+//!
+//!     export_html(&parsed, &mut output, false);
+//! }
+//! ```
+
 use std::io::Read;
 
 mod export;
